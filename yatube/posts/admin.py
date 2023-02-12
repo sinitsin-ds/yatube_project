cbf34e4b89b4ded50bfd116cf1e10fdc59f36b12
Дмитрий Синицын <sinitsin-ds@yandex.ru>
2023-02-12 13:32:15 +0300

    Убраны коментарии

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -3,8 +3,8 @@ from django.contrib import admin
 from .models import Post
 from .models import Group
 
+
 class PostAdmin(admin.ModelAdmin):
-    # Перечисляем поля, которые должны отображаться в админке
     list_display = (
         'pk',
         'text',
@@ -12,17 +12,11 @@ class PostAdmin(admin.ModelAdmin):
         'author',
         'group',
     )
-    # Добавляем возможность изменять поле group из списка постов
     list_editable = ('group',) 
-    # Добавляем интерфейс для поиска по тексту постов
     search_fields = ('text',) 
-    # Добавляем возможность фильтрации по дате
     list_filter = ('pub_date',)
-    # Это свойство сработает для всех колонок: где пусто — там будет эта строка 
     empty_value_display = '-пусто-'  
     
 
-# При регистрации модели Post источником конфигурации для неё назначаем
-# класс PostAdmin
 admin.site.register(Group)  
 admin.site.register(Post, PostAdmin)
--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -4,25 +4,27 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class Group(models.Model):
-    title = models.CharField(max_length=50)
-    slug = models.SlugField(max_length=20, unique=True)
-    description = models.TextField()
+    title = models.CharField(max_length=50, verbose_name='имя группы')
+    slug = models.SlugField(max_length=20, unique=True, verbose_name='раздел группы')
+    description = models.TextField(verbose_name='описание группы')
 
     def __str__(self):
         return (self.title)
 
 class Post(models.Model):
-    text = models.TextField()
-    pub_date = models.DateTimeField(auto_now_add=True)
+    text = models.TextField(verbose_name='текст статьи')
+    pub_date = models.DateTimeField(auto_now_add=True, verbose_name='дата публикации')
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='posts'
+        related_name='posts',
+        verbose_name='автор'
     )
     group = models.ForeignKey(
         Group,
         on_delete=models.CASCADE,
         blank=True,
         null=True,
-        related_name='group_posts'
+        related_name='group_posts',
+        verbose_name='группа'
     )
